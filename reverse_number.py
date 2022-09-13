952d3e68679f8ae005a1d264cd99e5af55bd2e42
eugenyegorov <eugenyegorov@yandex-team.ru>
2022-09-13 13:10:33 +0300

    added the doc string

--- reverse_number.py
+++ reverse_number.py
@@ -4,10 +4,13 @@ class UnsupportedInput(Exception):
 
 def reverse_number(number: int) -> int:
     """
-
+    The method accepts a natural number and returns
+    another natural number that represents input’s reverse.
     """
     if not isinstance(number, int):
         raise UnsupportedInput(f'Expected integer, but {type(number)} given')
+    if number <= 0:
+        raise UnsupportedInput(f'Only natural numbers are supported')
     result = 0
     i = 1
     while True:
